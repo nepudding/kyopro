8ae298e1ef2977ee62e83bd5046695235d10ae0e
hash <17024120@mmm.muroran-it.ac.jp>
2019-11-04 04:40:31 +0900

    コンビネーションに使う

---
+++ comb.py
@@ -0,0 +1,21 @@
+MOD = 998244353         # 問題に合わせて適宜書き換え
+list_size = 10000010    # 問題に合わせて適宜書き換え
+
+f_list = [1] * list_size
+f_r_list = [1] * list_size
+
+for i in range(list_size - 1):
+	f_list[i + 1] = ((f_list[i] * (i+2)) % MOD)
+
+f_r_list[-1] = pow(f_list[-1], MOD - 2, MOD)
+
+for i in range(-2, -list_size-1, -1):
+	f_r_list[i] = ((f_r_list[i+1] * (list_size + 2 + i)) % MOD)
+
+def comb(n,r):
+    if r == 0:
+        return 1
+    return (((f_list[n - 1] * f_r_list[n - r - 1]) % MOD) * f_r_list[r - 1]) % MOD
+
+N = int(input())
+print(comb(N,N//2))
