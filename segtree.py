ef8b171ff03d8c1c47487ffcedadb0816f61393e
nepudding <mizuki.1218zx@gmail.com>
2020-02-13 01:53:44 +0900

    セグ木書いたよ

---
+++ segtree.py
@@ -0,0 +1,38 @@
+import math
+
+class segtree:
+    def __init__(self,size):
+        n = 1
+        while n < size:
+            n *= 2
+        self.node = [0]*(2*n-1)
+        self.begin = n
+
+    def update(self,dex,value):
+        dex += self.begin-1
+        self.node[dex] = value
+        while dex > 0:
+            dex = (dex-1)//2
+            self.node[dex] = self.node[dex*2+1] + self.node[dex*2+2]
+    
+    def segment(self,l,r):
+        l += self.begin-1
+        r += self.begin-1
+        ans = 0
+        while l <= r:
+            if l==0:
+                ans = self.node[0]
+                break
+            if l%2 == 0:
+                ans += self.node[l]
+            l//=2
+            if r%2 == 1:
+                ans += self.node[r]
+            r = r//2 -1
+        
+        return ans 
+
+    def view(self):
+        return  self.node
+
+
